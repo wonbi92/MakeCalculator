0e277dac5878abfcc732b41db60c524d26ace9c4
Wonbi <wonbi@Wonbiui-MacBookAir.local>
2022-07-18 23:02:36 +0900

    feat: 계산기 기능 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberOutputLabel: UILabel!
     
     var displayNumber: String = ""
-    var firstOperend: Int = 0
-    var secondOperend: Int = 0
+    var firstOperand: Double = 0
+    var secondOperand: Double = 0
     var result: String = ""
     var currentOperation: Operation = .unknwon
     let numberFormatter = NumberFormatter()
@@ -31,11 +31,11 @@ class ViewController: UIViewController {
     
     func formattingToDecimal(num: String) {
         numberFormatter.numberStyle = .decimal
-        if displayNumber.contains(".") {
-            guard let naturalNumber = numberFormatter.string(for: Int(displayNumber.components(separatedBy: ".")[0]) ?? 0) else { return }
+        if num.contains(".") {
+            guard let naturalNumber = numberFormatter.string(for: Int(num.components(separatedBy: ".")[0]) ?? 0) else { return }
             numberOutputLabel.text = naturalNumber + "." + num.components(separatedBy: ".")[1]
         } else {
-            numberOutputLabel.text = numberFormatter.string(for: Int(displayNumber) ?? 0)
+            numberOutputLabel.text = numberFormatter.string(for: Int(num) ?? 0)
         }
     }
 
@@ -50,8 +50,8 @@ class ViewController: UIViewController {
     
     @IBAction func tapClearButton(_ sender: UIButton) {
         displayNumber = ""
-        firstOperend = 0
-        secondOperend = 0
+        firstOperand = 0
+        secondOperand = 0
         result = ""
         currentOperation = .unknwon
         numberOutputLabel.text = "0"
@@ -65,18 +65,56 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapDivideButton(_ sender: UIButton) {
+        operation(.Divide)
     }
     
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        operation(.Multiply)
     }
     
     @IBAction func tapSubtractButton(_ sender: UIButton) {
+        operation(.Subtract)
     }
     
     @IBAction func tapAddButton(_ sender: UIButton) {
+        operation(.Add)
     }
     
     @IBAction func tapEqualButton(_ sender: UIButton) {
+        operation(currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if currentOperation != .unknwon {
+            if !displayNumber.isEmpty {
+                secondOperand = Double(displayNumber) ?? 0
+                displayNumber = ""
+                let firstDoubleOperand = Double(firstOperand)
+                let secondDoubleOperand = Double(secondOperand)
+                switch currentOperation {
+                case .Add:
+                    result = "\(firstDoubleOperand + secondDoubleOperand)"
+                case .Subtract:
+                    result = "\(firstDoubleOperand - secondDoubleOperand)"
+                case .Multiply:
+                    result = "\(firstDoubleOperand * secondDoubleOperand)"
+                case .Divide:
+                    result = "\(firstDoubleOperand / secondDoubleOperand)"
+                default:
+                    break
+                }
+                if let result = Double(result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                firstOperand = Double(result) ?? 0
+                formattingToDecimal(num: result)
+            }
+            currentOperation = operation
+        } else {
+            firstOperand = Double(displayNumber) ?? 0
+            currentOperation = operation
+            displayNumber = ""
+        }
     }
 }
 
